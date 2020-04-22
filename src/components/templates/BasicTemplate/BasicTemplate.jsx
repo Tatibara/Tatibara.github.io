5c4ea214c6bbe54ced21fd8d7ed561194f9d1b97
Tatiana Baradari <8246564+Tatibara@users.noreply.github.com>
2020-04-22 12:05:37 +0200

    added react-cookie-consent

--- src/components/templates/BasicTemplate/BasicTemplate.jsx
+++ src/components/templates/BasicTemplate/BasicTemplate.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
+import CookieConsent from 'react-cookie-consent';
+import { Link } from 'react-router-dom';
+
+import NavigationUrls from '../../../routers/NavigationUrls';
 import Footer from '../../organisms/Footer/Footer';
 
 const BasicTemplate = ({ children }) => (
   <div>
     <div>{children}</div>
     <Footer />
+    <CookieConsent
+      buttonText="Akzeptieren"
+      style={{
+        opacity: '0.8',
+        fontSize: '13px',
+        letterSpacing: '1px',
+      }}
+      buttonStyle={{
+        fontSize: '13px',
+        letterSpacing: '1px',
+      }}
+    >
+      Diese Website verwendet Cookies, um die Benutzererfahrung zu verbessern.
+      Weitere Informationen erhalten Sie in unserer
+      {' '}
+      <Link to={NavigationUrls.dataProtectionPageUrl}>Datenschutzerklärung</Link>
+      .
+    </CookieConsent>
   </div>
 );
 
