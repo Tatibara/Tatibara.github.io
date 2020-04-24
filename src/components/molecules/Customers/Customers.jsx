783ca2d876aa00d2c2a91f8995672c3a1b41de92
Tatiana Baradari <8246564+Tatibara@users.noreply.github.com>
2020-04-24 13:33:25 +0200

    updated customers components

--- src/components/molecules/Customers/Customers.jsx
+++ src/components/molecules/Customers/Customers.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-import basilocomLogo from './basilicom_logo.png';
-import ergoDitLogo from './Ergo-DIT-logo.png';
-import frauenLoopLogo from './frauenlooplogo-2.jpg';
-import soniqServiceLogo from './soniq_services_logo.jpg';
+import basilocomLogo from './assets/basilicom_logo.png';
+import ergoDitLogo from './assets/Ergo-DIT-logo.png';
+import frauenLoopLogo from './assets/Frauenloop_logo.png';
+import {
+  ReactComponent as SoniqServiceLogo,
+} from './assets/soniq_services_logo.svg';
 import styles from './styles.module.scss';
 
 const Customers = () => (
   <div className={styles.customers}>
-    <img src={basilocomLogo} alt="Logo" className={styles.basilicom} />
-    <img src={ergoDitLogo} alt="Logo" className={styles.ergoDit} />
-    <img src={frauenLoopLogo} alt="Logo" className={styles.frauenLoop} />
-    <img src={soniqServiceLogo} alt="Logo" className={styles.soniqService} />
+    <img src={basilocomLogo} alt="Logo" className={`${styles.customerLogo} ${styles.basilicom}`} />
+    <img src={ergoDitLogo} alt="Logo" className={`${styles.customerLogo} ${styles.ergoDit}`} />
+    <img src={frauenLoopLogo} alt="Logo" className={`${styles.customerLogo} ${styles.frauenLoop}`} />
+    <SoniqServiceLogo alt="Logo" className={`${styles.customerLogo} ${styles.soniqService}`} />
 
   </div>
 );
